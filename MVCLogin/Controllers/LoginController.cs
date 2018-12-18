216236bf824e9599cdaba26354d68dbb4c5f2b7b
SabinSnider <nepalisoloplayer@gmail.com>
2018-12-18 10:41:40 +0545

    Importing Project

---
+++ MVCLogin/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+
+namespace MVCLogin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Authorize()
+        {
+            return View();
+        }
+    }
+}
